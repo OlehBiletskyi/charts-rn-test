341dc13c47c35b350926adaa2c9b0b23e6b49b44
OlehBiletskyi <ol.biletsky@gmail.com>
2023-01-04 16:28:36 +0200

    prepared app

---
+++ src/charts.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export const Charts = () => {
+    return (
+        <View>
+            <Text>
+                Тут можуть бути ваші графіки
+            </Text>
+        </View>
+    );
+};
